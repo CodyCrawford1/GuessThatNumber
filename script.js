26cd714579ac62d73053f2a2341406a465c8a90a
Cody Crawford <Cody.Crawford1010@gmail.com>
2022-03-01 13:12:09 -0600

    Set up game functionality

--- script.js
+++ script.js
@@ -45,5 +45,42 @@ while (restartGame){
     // Asks user to enter a guess in the range that they set
     prompt(`Please enter a guess from 1 to ${rangeNum}. You have ${attempts} attempt(s) left:`);
 
+    // Continues looping until the user guesses the correct number or runs ou of attempts (NOTE: Loops until a BREAK keyword is run)
+    while (true){
+    // Tries to convert the user's guess into a number
+    guess = parseInt(guess);
+
+    // Verifies the user's guess is number greater than zero and less than or equal to the range they set
+    while (!guess || guess < 1 || guess > rangeNum){
+        guess = parseInt(prompt(`Please enter a number from 1 to ${rangeNum}`));
+    }
+
+    // Removes an attempt 
+    attempts--;
+
+    // Checks if user guessed correctly. If so, then the game ends.
+    if (guess === randomNum){
+        alert(`CONGRATULATIONS YOU GUESSED THE CORRECT NUMBER: ${randomNum}`);
+        break;
+
+        // Checks if user has any attempts left. If not, then the game ends and the number is displayed to the user.
+    }   else if (attempts === 0) {
+        alert(`Sorry, but you have run out of attempts : ( The number was ${randomNum})`);
+        break;
+
+        // Checks if user's guess was too low and prompts user to guess again if so
+    } else if (guess < randomNum) {
+        guess = prompt(`Too low. You have ${attempts} attempt(s) left`);
+  
+        // The only other possibility is the user's guess was too high so the user is prompted again
+    } else {
+        guess = prompt(`Too high. You have ${attempts} attempt(s) left`);
+    }
+  
+
+    // break;
+    }
+
+
     break; 
 }
