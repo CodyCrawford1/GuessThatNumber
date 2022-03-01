fe99195ff7db71d8ce0485dcb734cddc2a45f576
Cody Crawford <Cody.Crawford1010@gmail.com>
2022-03-01 12:18:09 -0600

    Set up starting prompts. Range, and First Guess

--- script.js
+++ script.js
@@ -13,3 +13,37 @@ let attempts;
 let guess;
 // For storing the user's response to play again or not play again
 let playAgain;
+
+// Starting alert message
+alert(`Welcome to "GUESS THAT NUMBER!" Please click "OK" to start the game`);
+
+// Game restarts as long as the restartGame has a value of true
+while (restartGame){
+    // Asks user to enter a to set the upper bound for the random number that will be created.
+    rangeNum = prompt(`Please enter a maximum number for the range`);
+
+    // Using parseInt to attempt to convert the user's response into a number value
+    rangeNum = parseInt(rangeNum);
+
+    // Verifies the user's entry for a range number is a number greater than zero (NOTE: All number, positive or negative, have default boolean value of true, except for zero which has a default boolean value of false. Also, NaN has a default boolean value of false as well.)
+    while(!rangeNum || rangeNum < 1) {
+        rangeNum = prompt(enterNumText)
+        rangeNum = parseInt(rangeNum);
+    }
+
+    // Creates the random number using the range number entered by the user
+    randomNum = Math.floor(Math.random() * rangeNum) + 1;
+
+    // Prompts user to enter a number of attempts allowed (AKA number of guesses)
+    prompt(prompt(`Please enter a number of attempts allowed:`));
+
+    // Verifying the users entry for a number of attempts allowed is a number greater than zero and less than the range they set
+    while (!attempts || attempts < 1 || attempts > rangeNum) {
+        attempts = parseInt(prompt(`Please enter a number from 1 up to ${rangeNum - 1}:`));
+    }
+
+    // Asks user to enter a guess in the range that they set
+    prompt(`Please enter a guess from 1 to ${rangeNum}. You have ${attempts} attempt(s) left:`);
+
+    break; 
+}
